cbc691be3fd3a7203c8808782b19e0a32e9e409f
rirakib <rirrakib10@gmail.com>
2022-01-28 19:53:52 +0600

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class HomeController extends Controller
+{
+    //
+    public function redirect()
+    {
+        $userType = Auth::user()->userType;
+        if($userType == 1)
+        {
+            return view('admin.home');
+        }
+        else{
+            return view('user.home');
+        }
+    }
+
+    public function index()
+    {
+        return view('user.home');
+    }
+}
